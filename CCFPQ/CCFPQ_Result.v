6f60e84f510c2eeeff8d4ac0cb2adb91916c42f6
Roman Fedorov <Roman2x@yandex.ru>
2017-05-10 01:22:23 +0300

    Avoid apostrophes in names

--- CCFPQ/CCFPQ_Result.v
+++ CCFPQ/CCFPQ_Result.v
@@ -23,15 +23,15 @@ Section CFPQ_Res.
   Definition getLength VV AA xl1 xl2 (walk : D_walk VV AA xl1 xl2 VL AL) : nat := getLength' AL.
 
   Record CFPQ_Relational_query_result : Type := {
-    g_ : grammar;
-    CFPQ_ : var_EitherVertexNat_pair;
-    V_ : V_set;
-    A_ : A_set;
-    Graph_ : Digraph V_ A_; 
+    g_r : grammar;
+    CFPQ_r : var_EitherVertexNat_pair;
+    V_r : V_set;
+    A_r : A_set;
+    Graph_r : Digraph V_r A_r;
 
-    ntm_ : var := fst CFPQ_;
-    evnp_ : EitherVertexNat_pair := snd CFPQ_;
-    are_connected : exists (x1 x2 : Vertex), exists (conn : Vertex_conn V_ A_ x1 x2), True
+    ntm_r : var := fst CFPQ_r;
+    evnp_r : EitherVertexNat_pair := snd CFPQ_r;
+    are_connected : exists (x1 x2 : Vertex), exists (conn : Vertex_conn V_r A_r x1 x2), True
   }.
 
   Record CFPQ_Single_path_query_result : Type := {
@@ -39,42 +39,42 @@ Section CFPQ_Res.
     CFPQ : var_EitherVertexNat_pair;
     V : V_set;
     A : A_set;
-    Graph : Digraph V A; 
+    Graph : Digraph V A;
 
     ntm : var := fst CFPQ;
     evnp : EitherVertexNat_pair := snd CFPQ;
-    has_path : exists (x1 x2 : Vertex), exists (walk : D_walk V A x1 x2 _ _), 
+    has_path : exists (x1 x2 : Vertex), exists (walk : D_walk V A x1 x2 _ _),
                der g ntm (getLabels V A x1 x2 walk)
   }.
 
   Record CFPQ_Single_shortest_path_query_result : Type := {
-    g' : grammar;
-    CFPQ' : var_EitherVertexNat_pair;
-    V' : V_set;
-    A' : A_set;
-    Graph' : Digraph V' A'; 
+    g_s : grammar;
+    CFPQ_s : var_EitherVertexNat_pair;
+    V_s : V_set;
+    A_s : A_set;
+    Graph' : Digraph V_s A_s;
 
-    ntm' : var := fst CFPQ';
-    evnp' : EitherVertexNat_pair := snd CFPQ';
-    has_shortest_path : exists (x1 x2 : Vertex), exists (walk : D_walk V' A' x1 x2 _ _), 
-                        der g' ntm' (getLabels V' A' x1 x2 walk) -> 
-                        forall (other_walk : D_walk V' A' x1 x2 _ _), 
-                        getLength V' A' x1 x2 other_walk >= getLength V' A' x1 x2 walk
+    ntm_s : var := fst CFPQ_s;
+    evnp_s : EitherVertexNat_pair := snd CFPQ_s;
+    has_shortest_path : exists (x1 x2 : Vertex), exists (walk : D_walk V_s A_s x1 x2 _ _),
+                        der g_s ntm_s (getLabels V_s A_s x1 x2 walk) ->
+                        forall (other_walk : D_walk V_s A_s x1 x2 _ _),
+                        getLength V_s A_s x1 x2 other_walk >= getLength V_s A_s x1 x2 walk
   }.
 
   Record CFPQ_All_path_query_result : Type := {
-    g'' : grammar;
-    CFPQ'' : var_EitherVertexNat_pair;
-    V'' : V_set;
-    A'' : A_set;
-    Graph'' : Digraph V'' A''; 
+    g_a : grammar;
+    CFPQ_a : var_EitherVertexNat_pair;
+    V_a : V_set;
+    A_a : A_set;
+    Graph_a : Digraph V_a A_a;
 
-    ntm'' : var := fst CFPQ'';
-    evnp'' : EitherVertexNat_pair := snd CFPQ'';
-    all_paths : exists (all_pairs : list (prod Vertex Vertex)), 
+    ntm_a : var := fst CFPQ_a;
+    evnp_a : EitherVertexNat_pair := snd CFPQ_a;
+    all_paths : exists (all_pairs : list (prod Vertex Vertex)),
                 forall (pair_of_vertices : (prod Vertex Vertex)),
                 In pair_of_vertices all_pairs <-> 
-                exists (walk : D_walk V'' A'' (fst pair_of_vertices) (snd pair_of_vertices) _ _), 
-                  der g'' ntm'' (getLabels V'' A'' (fst pair_of_vertices) (snd pair_of_vertices) walk)
+                exists (walk : D_walk V_a A_a (fst pair_of_vertices) (snd pair_of_vertices) _ _),
+                  der g_a ntm_a (getLabels V_a A_a (fst pair_of_vertices) (snd pair_of_vertices) walk)
   }.
 End CFPQ_Res.
