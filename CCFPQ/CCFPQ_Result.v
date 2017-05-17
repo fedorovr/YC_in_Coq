46e6efd2e0b4d1cdac63ad4dd95d9dbdbc72b67e
Roman Fedorov <Roman2x@yandex.ru>
2017-05-17 16:35:29 +0300

    Add parse tree and 3 semantics of evaluation of CCFPQ; plus theorems for them

--- CCFPQ/CCFPQ_Result.v
+++ CCFPQ/CCFPQ_Result.v
@@ -1,5 +1,6 @@
-Require Import Definitions.
-Require Import Derivation.
+From mathcomp.ssreflect Require Import ssreflect ssrbool ssrnat eqtype ssrfun.
+
+Require Import CF_Definitions.
 Require Import Graph.
 Require Import CCFPQ.
 Require Import List.
@@ -8,105 +9,493 @@ Import ListNotations.
 Unset Implicit Arguments.
 Set Strict Implicit.
 
-Section CFPQ_Res.
-  Fixpoint getLabels' (arcs : A_list) : list symbol := match arcs with
+(* The main goal is to show that 3 sematics of CCFPQ evaluation can be *)
+(* derived from parse tree of a graph.                                 *)
+
+Inductive Parse_tree : Grammar -> V_set -> A_set -> Vertex ->
+                       Vertex -> var -> Prop :=
+  | Leaf : forall (g : Grammar) (V : V_set) (A : A_set) (x1 x2 : Vertex)
+                  (v : var) (t : ter),
+      V x1 -> V x2 -> In (Rt v t) g -> A (A_ends x1 x2 t) ->
+        Parse_tree g V A x1 x2 v
+  | Empty_leaf : forall (g : Grammar) (V : V_set) (A : A_set) (x : Vertex)
+                        (v : var) (e : eps),
+      V x -> In (Re v e) g -> Parse_tree g V A x x v
+  | Node : forall (g : Grammar) (V : V_set) (A : A_set) (x1 x2 x3 : Vertex)
+                  (v v1 v2 : var),
+      V x1 -> V x2 -> V x3 -> In (Rv v v1 v2) g ->
+        Parse_tree g V A x1 x2 v1 -> Parse_tree g V A x2 x3 v2 ->
+          Parse_tree g V A x1 x3 v.
+
+(* First semantic is relation. *)
+
+Inductive Derivability_relation : Grammar -> V_set -> A_set ->
+                                  Vertex -> Vertex -> var -> Prop :=
+  | Empty_rule : forall (g : Grammar) (V : V_set) (A : A_set) (x: Vertex)
+                        (v: var) (e: eps),
+       V x -> In (Re v e) g -> Derivability_relation g V A x x v
+  | Arc_with_ter_label : forall (g : Grammar) (V : V_set) (A : A_set)
+                                (x1 x2 : Vertex) (v : var) (t : ter),
+       V x1 -> V x2 -> In (Rt v t) g -> A (A_ends x1 x2 t) ->
+         Derivability_relation g V A x1 x2 v
+  | Rule_application : forall (g : Grammar) (V : V_set) (A : A_set)
+                       (x1 x2: Vertex) (v v1 v2 : var),
+       (exists (x' : Vertex), V x1 -> V x2 -> V x' -> In (Rv v v1 v2) g ->
+         Derivability_relation g V A x1 x' v1 /\
+           Derivability_relation g V A x' x2 v2) ->
+       Derivability_relation g V A x1 x2 v.
+
+Theorem Parse_tree_to_relation : forall (g : Grammar) (V : V_set) (A : A_set)
+                                        (x1 x2 : Vertex) (v : var),
+        Parse_tree g V A x1 x2 v -> Derivability_relation g V A x1 x2 v.
+Proof.
+  move => _g _V _A _x1 _x2 _v pt.
+  induction pt.
+
+  + apply: Arc_with_ter_label.
+    done.
+    done.
+    exact: H1.
+    done.
+
+  + apply: Empty_rule.
+    done.
+    done.
+
+  + apply: Rule_application.
+    exists x2.
+    move => vx1 vx3 vx2 hasRule.
+    split.
+    exact: IHpt1.
+    exact: IHpt2.
+Qed.
+
+(* Second semantic is single path. *)
+
+Fixpoint get_labels (arcs : list Arc) : list ter := match arcs with
+  | [] => []
+  | (A_ends _ _ l)::t => l::(get_labels t)
+end.
+
+Inductive D_walk : Vertex -> Vertex -> list Vertex -> list Arc -> Prop :=
+  | DW_null : forall (x : Vertex), D_walk x x V_nil A_nil
+  | DW_step : forall (x y z : Vertex)
+                     (vl : list Vertex) (al : list Arc) (l : ter),
+      D_walk y z vl al -> D_walk x z (y :: vl) (A_ends x y l :: al).
+
+Lemma D_walk_append:
+  forall (x y z : Vertex) (vl1 vl2 : list Vertex) (al1 al2 : list Arc),
+    D_walk x y vl1 al1 -> D_walk y z vl2 al2 ->
+      D_walk x z (vl1 ++ vl2) (al1 ++ al2).
+Proof.
+  intros x y z vl vl' el el' Hw; elim Hw; simpl in |- *; intros.
+  trivial.
+  apply DW_step; auto.
+Qed.
+
+Definition get_labels_from_walk x1 x2 vl al (walk : D_walk x1 x2 vl al)
+           : list ter := get_labels al.
+
+Inductive Single_path_semantics : forall (g : Grammar) (V : V_set) (A : A_set)
+            (v : var) (x1 x2 : Vertex) (vl : list Vertex) (al : list Arc),
+          D_walk x1 x2 vl al -> Prop :=
+  | Empty_path : forall (g : Grammar) (V : V_set) (A : A_set) (x : Vertex)
+                        (v : var) (e : eps),
+      V x -> In (Re v e) g ->
+        Single_path_semantics g V A v x x V_nil A_nil (DW_null x)
+  | One_arc_path : forall (g : Grammar) (V : V_set) (A : A_set)
+                          (x1 x2 : Vertex) (v : var) (t : ter),
+      V x1 -> V x2 -> In (Rt v t) g -> A (A_ends x1 x2 t) ->
+        Single_path_semantics g V A v x1 x2 [x2] [A_ends x1 x2 t]
+          (DW_step x1 x2 x2 V_nil A_nil t (DW_null x2))
+  | Combine_path : forall (g : Grammar) (V : V_set) (A : A_set)
+               (x1 x2 x3 : Vertex) (v v1 v2 : var)
+               (al1 al2 : list Arc) (vl1 vl2 : list Vertex)
+               (dw1 : D_walk x1 x2 vl1 al1) (dw2 : D_walk x2 x3 vl2 al2),
+      V x1 -> V x2 -> V x3 -> In (Rv v v1 v2) g ->
+         Single_path_semantics g V A v1 x1 x2 vl1 al1 dw1 ->
+           Single_path_semantics g V A v2 x2 x3 vl2 al2 dw2 ->
+             Single_path_semantics g V A v x1 x3 (vl1 ++ vl2) (al1 ++ al2)
+               (D_walk_append x1 x2 x3 vl1 vl2 al1 al2 dw1 dw2).
+
+Lemma get_labels_is_linear : forall (al1 al2 : list Arc),
+        get_labels (al1 ++ al2) = get_labels al1 ++ get_labels al2.
+Proof.
+  move => al1 al2.
+  induction al1.
+
+  + simpl.
+    done.
+
+  + simpl.
+    case (a).
+    move => v1 v2 t.
+    rewrite IHal1.
+    done.
+Qed.
+
+Theorem Derives_single_path : forall (g : Grammar) (V : V_set) (A : A_set)
+            (v : var) (x1 x2 : Vertex) (vl : list Vertex) (al : list Arc)
+            (dw : D_walk x1 x2 vl al)
+            (p : Single_path_semantics g V A v x1 x2 vl al dw),
+            Der_ter_list g v (get_labels_from_walk x1 x2 vl al dw).
+Proof.
+  move => g V A v x1 x2 vla la dw sps.
+  induction sps.
+
+  + rewrite / get_labels_from_walk.
+    simpl.
+    apply: Der_eps.
+    done.
+
+  + rewrite / get_labels_from_walk.
+    simpl.
+    apply: Der_ter.
+    done.
+
+  + rewrite / get_labels_from_walk.
+    rewrite get_labels_is_linear.
+    apply: Der_var.
+    exact H2.
+    done.
+    done.
+Qed.
+
+Theorem Parse_tree_to_single_path : forall (g : Grammar) (V : V_set)
+    (A : A_set) (x1 x2 : Vertex) (v : var),
+    Parse_tree g V A x1 x2 v ->
+      exists (vl : list Vertex) (al : list Arc) (dw : D_walk x1 x2 vl al),
+        Single_path_semantics g V A v x1 x2 vl al dw.
+Proof.
+  move => g V A x1 x2 v pt.
+  induction pt.
+
+  + exists [x2],
+           [A_ends x1 x2 t],
+           (DW_step x1 x2 x2 V_nil A_nil t (DW_null x2)).
+    apply One_arc_path.
+    done.
+    done.
+    done.
+    done.
+
+  + exists [],
+           [],
+           (DW_null x).
+    apply: Empty_path.
+    done.
+    done.
+
+  + destruct IHpt1 as [vl1 [al1 [dw1 IHpt1]]].
+    destruct IHpt2 as [vl2 [al2 [dw2 IHpt2]]].
+    exists (vl1 ++ vl2),
+           (al1 ++ al2),
+           (D_walk_append x1 x2 x3 vl1 vl2 al1 al2 dw1 dw2).
+    apply: Combine_path.
+    done.
+    done.
+    done.
+    exact H2.
+    done.
+    done.
+Qed.
+
+(* Third semantic is all paths. *)
+
+Fixpoint get_pairs (al : list Arc) (all : list (list Arc)) : list (list Arc) :=
+   match all with
+     | [] => []
+     | l::t => (al ++ l)::(get_pairs al t)
+   end.
+
+Fixpoint get_all_pairs (all1 all2 : list (list Arc)) : list (list Arc) :=
+  match all1 with
     | [] => []
-    | (A_ends _ _ l)::t => (Ts l)::(getLabels' t)
+    | l::t => (get_pairs l all2) ++ (get_all_pairs t all2)
   end.
 
-  Fixpoint getLength' (arcs : A_list) : nat := match arcs with
-    | [] => 0
-    | arc::t => 1 + (getLength' t)
-  end.
+Inductive All_path_semantics : forall (g : Grammar) (V : V_set) (A : A_set)
+          (v : var) (x1 x2 : Vertex) (all : list (list Arc)), Prop :=
+  | Empty_paths : forall (g : Grammar) (V : V_set) (A : A_set) (x : Vertex)
+                         (v : var) (e : eps),
+      V x -> In (Re v e) g -> All_path_semantics g V A v x x [[]]
+  | One_arc_paths : forall (g : Grammar) (V : V_set) (A : A_set)
+                           (x1 x2 : Vertex) (v : var) (t : ter),
+      V x1 -> V x2 -> In (Rt v t) g -> A (A_ends x1 x2 t) ->
+        All_path_semantics g V A v x1 x2 [[A_ends x1 x2 t]]
+  | Combine_paths : forall (g : Grammar) (V : V_set) (A : A_set)
+                           (x1 x2 x3 : Vertex) (v v1 v2 : var)
+                           (all1 all2 : list (list Arc)),
+      V x1 -> V x2 -> V x3 -> In (Rv v v1 v2) g ->
+        All_path_semantics g V A v1 x1 x2 all1 ->
+          All_path_semantics g V A v2 x2 x3 all2 ->
+            All_path_semantics g V A v x1 x3 (get_all_pairs all1 all2).
 
-  Variable VL : V_list.
-  Variable AL : A_list.
-  Definition getLabels VV AA xl1 xl2 (walk : D_walk VV AA xl1 xl2 VL AL) : list symbol := getLabels' AL.
-  Definition getLength VV AA xl1 xl2 (walk : D_walk VV AA xl1 xl2 VL AL) : nat := getLength' AL.
+Theorem Parse_tree_to_all_path : forall (g : Grammar) (V : V_set)
+    (A : A_set) (x1 x2 : Vertex) (v : var),
+    Parse_tree g V A x1 x2 v ->
+      exists (all : list (list Arc)), All_path_semantics g V A v x1 x2 all.
+Proof.
+  move => g V A x1 x2 v pt.
+  induction pt.
 
-  Fixpoint grammar_has_ter (g: grammar) (tr : ter) : Prop :=
-    match g with
-      | [] => False
-      | (R _ ph)::t => or (In (Ts tr) ph) (grammar_has_ter t tr)
-    end.
+  + exists [[A_ends x1 x2 t]].
+    apply One_arc_paths.
+    done.
+    done.
+    done.
+    done.
 
-  Inductive Derivability_relation : grammar -> V_set -> A_set ->
-                                    Vertex -> Vertex -> symbol -> Prop :=
-    | Empty_rule : forall (g : grammar) (V : V_set) (A : A_set) (x: Vertex) (s: symbol),
-         V x -> (exists (v: var), Vs v = s -> In (R v []) g) -> Derivability_relation g V A x x s
-    | Arc_with_ter_label : forall (g : grammar) (V : V_set) (A : A_set) (x1 x2: Vertex) (s: symbol),
-         V x1 -> V x2 -> (exists (t: ter), Ts t = s -> grammar_has_ter g t -> A (A_ends x1 x2 t)) ->
-         Derivability_relation g V A x1 x2 s
-    (* Given a non-terminal, check that exist list of vertices, that every symbol in right *)
-    (* hand side of a rule (for given non-terminal) consists in this derivability relation *)
-    (* with appropriate pair of vertices, i.e. symbol at i-th position should consist in   *)
-    (* derivability relation with i-th and (i+1)-th vertices accordingly.                  *)
-    | Rule_application : forall (g : grammar) (V : V_set) (A : A_set) (x1 x2: Vertex) (s: symbol),
-         V x1 -> V x2 ->
-           (exists (v: var) (ph : phrase) (vl : V_list),
-             Vs v = s -> In (R v ph) g -> 1 + length vl = length ph ->
-               (forall (idx : nat), idx < length ph -> (
-                 exists (x1' x2' : Vertex) (s' : symbol),
-                 nth_error (x1::vl ++ [x2]) idx = Some x1' ->
-                 nth_error (x1::vl ++ [x2]) (idx + 1) = Some x2' ->
-                 nth_error ph idx = Some s' ->
-                 Derivability_relation g V A x1' x2' s'))) ->
-         Derivability_relation g V A x1 x2 s.
+  + exists [[]].
+    eapply Empty_paths.
+    done.
+    exact H0.
 
-  Record CFPQ_Relational_query_result : Type := {
-    g_r : grammar;
-    CFPQ_r : var_EitherVertexNat_pair;
-    V_r : V_set;
-    A_r : A_set;
-    Graph_r : Digraph V_r A_r;
+  + destruct IHpt1 as [all1 aps1].
+    destruct IHpt2 as [all2 aps2].
+    exists (get_all_pairs all1 all2).
+    eapply Combine_paths.
+    done.
+    exact H0.
+    done.
+    exact H2.
+    done.
+    done.
+Qed.
 
-    ntm_r : var := fst CFPQ_r;
-    evnp_r : EitherVertexNat_pair := snd CFPQ_r;
-    are_in_derivability_relation : exists (x1 x2 : Vertex),
-      Derivability_relation g_r V_r A_r x1 x2 (Vs ntm_r)
-  }.
+Lemma app_is_associative_inside_in :
+      forall (x : list Arc) (a b c : list (list Arc)),
+             In x (a ++ b ++ c) -> In x ((a ++ b) ++ c).
+Proof.
+  move => x a b c.
+  remember (a ++ b ++ c) as abc.
+  rewrite app_assoc in Heqabc.
+  rewrite Heqabc.
+  done.
+Qed.
 
-  Record CFPQ_Single_path_query_result : Type := {
-    g : grammar;
-    CFPQ : var_EitherVertexNat_pair;
-    V : V_set;
-    A : A_set;
-    Graph : Digraph V A;
+Lemma app_is_associative_inside_in' :
+      forall (x : list Arc) (a b c : list (list Arc)),
+             In x ((a ++ b) ++ c) -> In x (a ++ b ++ c).
+Proof.
+  move => x a b c.
+  remember (a ++ b ++ c) as abc.
+  rewrite app_assoc in Heqabc.
+  rewrite Heqabc.
+  done.
+Qed.
 
-    ntm : var := fst CFPQ;
-    evnp : EitherVertexNat_pair := snd CFPQ;
-    has_path : exists (x1 x2 : Vertex), exists (walk : D_walk V A x1 x2 _ _),
-               der g ntm (getLabels V A x1 x2 walk)
-  }.
+Lemma app_nil_inside_in : forall (x : list Arc) (a b c : list (list Arc)),
+               In x (a ++ b ++ c) -> In x (a ++ b ++ c ++ []).
+Proof.
+  move => x a b c.
+  remember (a ++ b ++ c ++ []) as abcn.
+  rewrite app_nil_r in Heqabcn.
+  rewrite Heqabcn.
+  done.
+Qed.
 
-  Record CFPQ_Single_shortest_path_query_result : Type := {
-    g_s : grammar;
-    CFPQ_s : var_EitherVertexNat_pair;
-    V_s : V_set;
-    A_s : A_set;
-    Graph' : Digraph V_s A_s;
+Lemma in_cons_disj : forall (l a : list Arc) (ll : list (list Arc)),
+             In l ll \/ a = l -> In l (a :: ll).
+Proof.
+  move => l a ll stm.
+  destruct stm as [l_in_ll | a_is_l].
+  simpl.
+  right.
+  done.
+  simpl.
+  left.
+  done.
+Qed.
 
-    ntm_s : var := fst CFPQ_s;
-    evnp_s : EitherVertexNat_pair := snd CFPQ_s;
-    has_shortest_path : exists (x1 x2 : Vertex), exists (walk : D_walk V_s A_s x1 x2 _ _),
-                        der g_s ntm_s (getLabels V_s A_s x1 x2 walk) ->
-                        forall (other_walk : D_walk V_s A_s x1 x2 _ _),
-                        getLength V_s A_s x1 x2 other_walk >= getLength V_s A_s x1 x2 walk
-  }.
+Lemma in_app_cons_disj : forall (x b0 : list Arc) (a b : list (list Arc)),
+             In x (a ++ b) \/ In x (a ++ [b0]) -> In x (a ++ b0 :: b).
+Proof.
+  move => x b0 a.
+  induction a.
 
-  Record CFPQ_All_path_query_result : Type := {
-    g_a : grammar;
-    CFPQ_a : var_EitherVertexNat_pair;
-    V_a : V_set;
-    A_a : A_set;
-    Graph_a : Digraph V_a A_a;
+  + move => b.
+    simpl.
+    firstorder.
 
-    ntm_a : var := fst CFPQ_a;
-    evnp_a : EitherVertexNat_pair := snd CFPQ_a;
-    all_paths : exists (all_pairs : list (prod Vertex Vertex)),
-                forall (pair_of_vertices : (prod Vertex Vertex)),
-                In pair_of_vertices all_pairs <-> 
-                exists (walk : D_walk V_a A_a (fst pair_of_vertices) (snd pair_of_vertices) _ _),
-                  der g_a ntm_a (getLabels V_a A_a (fst pair_of_vertices) (snd pair_of_vertices) walk)
-  }.
-End CFPQ_Res.
+  + move => b stm.
+    destruct stm as [x_in_aa0'b | x_im aa0'b0].
+    firstorder.
+    firstorder.
+Qed.
+
+Lemma app_is_commutative_inside_in2 :
+      forall (x : list Arc) (a b : list (list Arc)),
+             In x (a ++ b) -> In x (b ++ a).
+Proof.
+  move => x a.
+  induction a.
+
+  + move => b.
+    simpl.
+    move => x_in_b.
+    remember (b ++ []) as bn.
+    rewrite app_nil_r in Heqbn.
+    rewrite Heqbn.
+    done.
+
+  + move => b x_in.
+    simpl in x_in.
+    destruct x_in as [x_is_a | x_in_a0b].
+    apply in_or_app.
+    right.
+    apply in_cons_disj.
+    right.
+    done.
+    apply IHa in x_in_a0b.
+    apply in_app_cons_disj.
+    left.
+    done.
+Qed.
+
+Lemma app_is_commutative_inside_in3 :
+      forall (x : list Arc) (a b c : list (list Arc)),
+             In x (a ++ b ++ c) -> In x (b ++ c ++ a).
+Proof.
+  move => x a b c x_in_abc.
+  apply app_is_commutative_inside_in2 in x_in_abc.
+  apply app_is_associative_inside_in' in x_in_abc.
+  done.
+Qed.
+
+Lemma get_all_pairs_cons : forall (l x : list Arc) (ll1 ll2 : list (list Arc)),
+  In x (get_all_pairs (l::ll1) ll2) ->
+    In x (get_all_pairs ll1 ll2) \/ In x (get_all_pairs [l] ll2).
+Proof.
+  move => l x ll1.
+  induction ll1.
+
+  + move => ll2 conc.
+    simpl.
+    right.
+    done.
+
+  + move => ll2 conc.
+    simpl.
+    simpl in conc.
+    apply in_app_or.
+    apply app_is_associative_inside_in.
+    apply app_nil_inside_in.
+    apply app_is_commutative_inside_in3.
+    done.
+Qed.
+
+Lemma get_all_pairs_single : forall (a x : list Arc) (ll : list (list Arc)),
+  In x (get_all_pairs [a] ll) ->
+       (exists (b : list Arc), In b ll /\ (x = (a ++ b))).
+Proof.
+  move => a x ll.
+  induction ll.
+
+  + move => stm.
+    simpl in stm.
+    done.
+
+  + move => stm.
+    simpl in stm.
+    destruct stm as [x_is_sum | x_in_concat].
+    exists a0.
+    split.
+    apply in_cons_disj.
+    right.
+    done.
+    done.
+    simpl in IHll.
+    apply IHll in x_in_concat.
+    destruct x_in_concat as [b bstm].
+    destruct bstm as [b_in_ll x_is_sum].
+    exists b.
+    split.
+    apply in_cons_disj.
+    left.
+    done.
+    done.
+Qed.
+
+Lemma exists_in_get_all_pairs :
+  forall (l : list Arc) (ll1 ll2 : list (list Arc)),
+    In l (get_all_pairs ll1 ll2) ->
+         (exists (l1 l2 : list Arc), In l1 ll1 /\ In l2 ll2 /\ l = (l1 ++ l2)).
+Proof.
+  move => l ll1.
+  induction ll1.
+
+  + move => ll2 conc_in_pairs.
+    simpl in conc_in_pairs.
+    done.
+
+  + move => ll2 conc_in_pairs.
+    apply get_all_pairs_cons in conc_in_pairs.
+    destruct conc_in_pairs as [in1 | in2].
+    apply IHll1 in in1.
+    destruct in1 as [l1 [l2 stm]].
+    destruct stm as [l1_in [l2_in l_is_sum]].
+    exists l1.
+    exists l2.
+    split.
+    apply in_cons_disj.
+    left.
+    done.
+    split.
+    done.
+    done.
+    apply get_all_pairs_single in in2.
+    destruct in2 as [l' stm].
+    destruct stm as [l'_in l_is_sum].
+    exists a.
+    exists l'.
+    split.
+    apply in_cons_disj.
+    right.
+    done.
+    split.
+    done.
+    done.
+Qed.
+
+Theorem derives_all_path : forall (g : Grammar) (V : V_set) (A : A_set)
+         (v : var) (x1 x2 : Vertex) (all : list (list Arc))
+         (aps : All_path_semantics g V A v x1 x2 all),
+         forall (al : list Arc), In al all -> Der_ter_list g v (get_labels al).
+Proof.
+  move => g V A v x1 x2 all aps.
+  induction aps.
+
+  + move => conc contains.
+    case contains.
+    move => empty.
+    rewrite <- empty.
+    simpl.
+    apply: Der_eps.
+    done.
+    rewrite / In.
+    done.
+
+  + move => conc contains.
+    case contains.
+    move => al_is.
+    rewrite <- al_is.
+    simpl.
+    apply: Der_ter.
+    done.
+    rewrite / In.
+    done.
+
+  + move => conc contains.
+    apply exists_in_get_all_pairs in contains.
+    destruct contains as [l1 [l2 contains]].
+    destruct contains as [l1_in_all1 [l2_in_all2 contains]].
+    rewrite contains.
+    rewrite get_labels_is_linear.
+    apply: Der_var.
+    exact H2.
+    apply IHaps1.
+    done.
+    apply IHaps2.
+    done.
+Qed.
